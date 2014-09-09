057b2bcf6f024d239548df8df0264740f4702d6f
alan bount <alan@zeroasterisk.com>
2014-09-09 16:52:33 -0400

    fixes to package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
-  summary: "Cordova enabled background geolocation, so your Meteor Cordova app can update location even when closed/suspended",
-  version: "0.1.0",
-  name: "zeroasterisk:meteor-cordova-geolocation-background",
+  summary: "Cordova enabled background geolocation, even when closed/suspended",
+  version: "0.0.1",
+  name: "zeroasterisk:cordova-geolocation-background",
   git: "https://github.com/zeroasterisk/meteor-cordova-geolocation-background"
 });
 
@@ -14,6 +14,7 @@ Cordova.depends({
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.2-rc1');
+  api.use('underscore');
   api.addFiles('cordova-geolocation.js', 'client');
   api.addFiles('cordova-geolocation-background.js', 'client');
 });
