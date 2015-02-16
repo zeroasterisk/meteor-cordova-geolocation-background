277c3df4baad6bcc15d0134b48e9f884f8fe2e0d
alan bount <alan@zeroasterisk.com>
2015-02-16 14:28:15 -0500

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Cordova enabled background geolocation, even when closed/suspended",
-  version: "0.0.5",
+  version: "0.0.6",
   name: "zeroasterisk:cordova-geolocation-background",
   git: "https://github.com/zeroasterisk/meteor-cordova-geolocation-background"
 });
